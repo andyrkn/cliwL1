60e38f9e6685764f7e10e55020a72372f6351841
Mihaila Andrei <andyfbgm@gmail.com>
2018-10-11 18:33:46 +0300

    feedback update
    - moved button events to container and using his event to track clicks
    - cached wrapper and buttons for less memory
    - changing div properties instead of button properties

--- home.js
+++ home.js
@@ -1,21 +1,28 @@
-cardsList = [];
-ithSelectedCard = 0;
-secondsLevel = 5;
-secondsLeft = 0;
+// --------------------cache meme----------------------
+let wrapper;
+let buttons;
+// ----------------------------------------------------
+let cardsList = [];
+let ithSelectedCard = 0;
 
-currentRound = 0;
-orderIndex = 0;
+let currentRound = 0;
 
-numberOfCards = 5;
+let secondsLevel = 5;
+let secondsLeft = 0;
+let numberOfCards = 2;
 
+let secondsLeftDisplay = 5;
+let streakDisplay = 0;
+
+// painting order
+let orderIndex = 0;
 // time in ms
-fadeoutTimer = 1000;
+const fadeoutTimer = 1000;
 var mainInterval, highlightingInterval;
 
 function recolor() {
-    for (let i = 1; i <= 9; i++) {
-        let name = 'bt' + i;
-        document.getElementById(name).style.backgroundColor = 'yellow';
+    for (let i = 0; i <= 8; i++) {
+        buttons[i].style.backgroundColor = 'yellow';
     }
 }
 
@@ -36,20 +43,30 @@ function manageDifficultyLevels() {
         secondsLevel = 3;
     }
 
-    document.getElementById('htime').innerHTML = secondsLevel;
+    secondsLeftDisplay.innerHTML = secondsLevel;
 }
 
 function init() {
+    // find a way to do this in a more elegant way
+    // load elements once and prepare event delegation
+    if (!wrapper) {
+        document.getElementById('wrapper').addEventListener("click", function (e) { containerClick(e); })
+        wrapper = document.getElementById('wrapper');
+        // will have to compensate sometimes with buttonid - 1 since this will go from 0-8 and initially i've coded 1-9
+        buttons = document.getElementsByClassName('btn');
+        secondsLeftDisplay = document.getElementById('htime');
+        streakDisplay = document.getElementById('streak');
+    }
+
     recolor();
-    //manageDifficultyLevels();
     changeCardsClickable('none');
     numberOfCards = 2 + Number(currentRound);
-    secondsLeft = secondsLevel + Number(currentRound)/2;
+    secondsLeft = secondsLevel + Number(currentRound) / 2;
     ithSelectedCard = 0;
     cardsList = [];
     orderIndex = 0;
-    document.getElementById('htime').innerHTML = secondsLeft;
-    document.getElementById('streak').innerHTML = 'Streak: ' + currentRound;
+    secondsLeftDisplay.innerHTML = secondsLeft;
+    streakDisplay.innerHTML = 'Streak: ' + currentRound;
 }
 
 function reset() {
@@ -74,42 +91,41 @@ function start() {
 
 
     // begin showing the card order
-    document.getElementById('bt' + cardsList[0]).style.backgroundColor = 'red';
+    buttons[cardsList[0] - 1].style.backgroundColor = 'red';
     highlightingInterval = setInterval(highlightCard, fadeoutTimer);
 }
 
 function highlightCard() {
     // console.log(orderIndex + ' <- order index');
-    document.getElementById('bt' + cardsList[orderIndex]).style.backgroundColor = 'yellow';
+    buttons[cardsList[orderIndex] - 1].style.backgroundColor = 'yellow';
 
     if (orderIndex < cardsList.length - 1) {
         orderIndex = Number(orderIndex) + 1;
-        document.getElementById('bt' + cardsList[orderIndex]).style.backgroundColor = 'red';
+        buttons[cardsList[orderIndex] - 1].style.backgroundColor = 'red';
     }
     else {
-        document.getElementById('bt' + cardsList[orderIndex]).style.backgroundColor = 'yellow';
+        buttons[cardsList[orderIndex] - 1].style.backgroundColor = 'yellow';
         clearInterval(highlightingInterval);
         countdown();
     }
 }
 
-//card onclick event, set it as green.
-function c1(button) {
-    document.getElementById(button.id).style.backgroundColor = 'green';
-    addelement(button.id);
+//card onclick event, set it as green and pass to checker.
+function containerClick(event) {
+    let buttonId = event.target.dataset.action;
+    buttons[buttonId - 1].style.backgroundColor = 'green';
+    addElement(buttonId);
 }
 
 // check if the card selected is correct, if not, stop the round and call reset
-function addelement(buttonId) {
+function addElement(buttonId) {
     // remove the color off the previous card
     if (ithSelectedCard > 0)
-        document.getElementById('bt' + cardsList[ithSelectedCard - 1]).style.backgroundColor = 'yellow';
+        buttons[cardsList[ithSelectedCard - 1] - 1].style.backgroundColor = 'yellow';
 
-    // extract id from button eg: { id = bt9 -> str = 9 }
-    str = buttonId.substring(buttonId.length - 1, buttonId.length);
     // check if the order is correct
-    if (cardsList[ithSelectedCard] == str) {
-        console.log(str + ' click correct');
+    if (cardsList[ithSelectedCard] == buttonId) {
+        console.log(buttonId + ' click correct');
         if (ithSelectedCard == numberOfCards - 1) {
             // alert('you win'); 
             currentRound = Number(currentRound) + 1;
@@ -129,10 +145,7 @@ function addelement(buttonId) {
 
 //set the cards clickable or not, with the help of 'all' and 'none'
 function changeCardsClickable(incPointerEvent) {
-    for (let i = 1; i <= 9; i++) {
-        let name = 'bt' + i;
-        document.getElementById(name).style.pointerEvents = incPointerEvent;
-    }
+    wrapper.style.pointerEvents = incPointerEvent;
 }
 
 pauseActive = 0;
@@ -152,13 +165,13 @@ function countdown() {
     changeCardsClickable('all');
     // main countdown responsable with the game
     mainInterval = setInterval(function () {
-        if (Number(secondsLeft) == 0) {
+        if (Number(secondsLeft) <= 0) {
             clearInterval(mainInterval);
             alert('defeat');
             reset();
         } else {
             secondsLeft = Number(secondsLeft) - 1 + Number(pauseActive);
-            document.getElementById('htime').innerHTML = secondsLeft;
+            secondsLeftDisplay.innerHTML = secondsLeft;
         }
     }, 1000);
 }
